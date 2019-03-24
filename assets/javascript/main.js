76ea45399b87d8c147aabd76860efe5fe0c8d224
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-03-23 23:30:01 -0600

    Discovered I was using the server API, working on Google Places Library API now

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,6 @@
 var googleKey = "AIzaSyBLJrE6KEfUSM16_1CCc0W_QFNSWDbkkx0";
+var nextPage;
+src="https://maps.googleapis.com/maps/api/js?key="+googleKey+"&libraries=places";
 
 // * On Zip code input run API commands
 $("#submit").on("click", function (event) {
@@ -15,6 +17,8 @@ $("#submit").on("click", function (event) {
         success: function (response) {
             console.log("TCL: locationUrl", locationUrl);
             console.log("TCL: response", response.results[0].geometry.location);
+
+
 
             var lat = response.results[0].geometry.location.lat;
             console.log("TCL: lat", lat);
@@ -44,19 +48,25 @@ $("#submit").on("click", function (event) {
 
 
 // *Testing out APIs in a separate spot to keep making progress
-var lat = 40.8911434;
-var long = -111.8925837;
+var lat = "40.8911434";
+var long = "-111.8925837";
 
-var placesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + long + "&radius=5000&type=restaurant&googleKey=" + googleKey;
+var placesUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBLJrE6KEfUSM16_1CCc0W_QFNSWDbkkx0";
 
+service = new google.maps.places.PlacesService(map);
+service.nearbySearch(request, callback);
 
 $.ajax({
-    method: "GET",
-    type: "json",
-    url: placesUrl,
-    // Accept: "*/*",
+    url: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBLJrE6KEfUSM16_1CCc0W_QFNSWDbkkx0",
+    type: 'GET',
     success: function (response) {
         console.log("TCL: response", response);
+        nextPage = response.next_page_token;
+        console.log("TCL: nextPage", nextPage);
 
-    }
+    },
+    error: function () {
+        alert('Failed!');
+    },
+
 });
