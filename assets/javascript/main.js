0a62aa8d6da9f234dfd8a4042e689874cb917410
Cameron Larrabee <cameronlarrabee@yahoo.com>
2019-03-30 12:42:35 -0600

    restaurant row size and fixed, and fixed liek row

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -7,27 +7,27 @@ src = "https://maps.googleapis.com/maps/api/js?key=" + googleKey + "&libraries=p
 
 
 $(document).on("click", "#dislike", function () {
-    if (!noUsersCity) {
-        dilikeArr.push(name);
-        restIndex++;
-        if (placeArr[restIndex] === undefined) {
-            $("#restaurant").empty().addClass("expand").append("<button> Keep searching?");
-        }
-        placeDetails(placeArr[restIndex]);
+  if (!noUsersCity) {
+    dilikeArr.push(name);
+    restIndex++;
+    if (placeArr[restIndex] === undefined) {
+      $("#restaurant").empty().addClass("expand").append("<button> Keep searching?");
     }
+    placeDetails(placeArr[restIndex]);
+  }
 });
 $(document).on("click", "#like", function () {
 
-    if (!noUsersCity) {
-        $("#restaurant").children(".card").addClass("liked" + likeIndex);
-        likeArr.push(placeArr[restIndex]);
-        likedDiv();
-        restIndex++;
-        if (placeArr[restIndex] === undefined) {
-            $("#restaurant").empty().addClass("expand").append("<button> Keep searching?");
-        }
-        placeDetails(placeArr[restIndex]);
+  if (!noUsersCity) {
+    $("#restaurant").children(".card").addClass("liked" + likeIndex);
+    likeArr.push(placeArr[restIndex]);
+    likedDiv();
+    restIndex++;
+    if (placeArr[restIndex] === undefined) {
+      $("#restaurant").empty().addClass("expand").append("<button> Keep searching?");
     }
+    placeDetails(placeArr[restIndex]);
+  }
 });
 
 
@@ -36,31 +36,31 @@ $(document).on("click", "#like", function () {
 
 $("#submit").on("click", function (event) {
 
-    noUsersCity = false;
-    event.preventDefault();
+  noUsersCity = false;
+  event.preventDefault();
 
-    var autocomplete = $("#autocomplete").val().trim();
-    var locationUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + autocomplete + "&key=" + googleKey;
+  var autocomplete = $("#autocomplete").val().trim();
+  var locationUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + autocomplete + "&key=" + googleKey;
 
 
-    //* API call for retrieving longitude and latitude from zip
-    $.ajax({
-        url: locationUrl,
-        type: "json",
-        method: "GET",
-        success: function (response) {
+  //* API call for retrieving longitude and latitude from zip
+  $.ajax({
+    url: locationUrl,
+    type: "json",
+    method: "GET",
+    success: function (response) {
 
-            lat = response.results[0].geometry.location.lat;
-            long = response.results[0].geometry.location.lng;
+      lat = response.results[0].geometry.location.lat;
+      long = response.results[0].geometry.location.lng;
 
-            initializeSearch();
+      initializeSearch();
 
-        },
+    },
 
-        error: function (error) {
-            console.log(error);
-        }
-    });
+    error: function (error) {
+      console.log(error);
+    }
+  });
 });
 
 
@@ -72,28 +72,28 @@ var map, places, infoWindow;
 var markers = [];
 var autocomplete;
 var countryRestrict = {
-    'country': 'us'
+  'country': 'us'
 };
 var MARKER_PATH = 'https://developers.google.com/maps/documentation/javascript/images/marker_green';
 var hostnameRegexp = new RegExp('^https?://.+?/');
 
 var countries = {
-    'us': {
-        center: {
-            lat: 37.1,
-            lng: -95.7
-        },
-        zoom: 3
+  'us': {
+    center: {
+      lat: 37.1,
+      lng: -95.7
     },
+    zoom: 3
+  },
 };
 
 autocomplete = new google.maps.places.Autocomplete(
-    /** @type {!HTMLInputElement} */
-    (
-        document.getElementById('autocomplete')), {
-        types: ['(cities)'],
-        componentRestrictions: countryRestrict
-    });
+  /** @type {!HTMLInputElement} */
+  (
+    document.getElementById('autocomplete')), {
+    types: ['(cities)'],
+    componentRestrictions: countryRestrict
+  });
 places = new google.maps.places.PlacesService(map);
 
 autocomplete.addListener('place_changed', onPlaceChanged);
@@ -101,127 +101,127 @@ autocomplete.addListener('place_changed', onPlaceChanged);
 // *Copied from "Find Places Nearby" https://developers.google.com/maps/documentation/javascript/places#place_search_requests
 
 function initializeSearch() {
-    var location = new google.maps.LatLng(lat, long);
-    radius = 500;
-    restIndex = 0;
+  var location = new google.maps.LatLng(lat, long);
+  radius = 500;
+  restIndex = 0;
 
-    tempArr = [];
-    placeArr = [];
-    $("#restaurant").empty();
+  tempArr = [];
+  placeArr = [];
+  $("#restaurant").empty();
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: location,
-        zoom: 15
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 15
+  });
 
-    var request = {
-        location: location,
-        radius: radius,
-        type: ['restaurant']
-    };
+  var request = {
+    location: location,
+    radius: radius,
+    type: ['restaurant']
+  };
 
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
 
 var radius;
 var tempArr = [];
 
 function expandSearch() {
-    var location = new google.maps.LatLng(lat, long);
+  var location = new google.maps.LatLng(lat, long);
 
-    for (var i = 0; i < placeArr.length; i++) {
-        var element = placeArr[i];
-        tempArr.push(element);
-    }
+  for (var i = 0; i < placeArr.length; i++) {
+    var element = placeArr[i];
+    tempArr.push(element);
+  }
 
-    radius += 250;
-    restIndex = 0;
+  radius += 250;
+  restIndex = 0;
 
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: location,
-        zoom: 15
-    });
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: location,
+    zoom: 15
+  });
 
-    var request = {
-        location: location,
-        radius: radius,
-        type: ['restaurant']
-    };
+  var request = {
+    location: location,
+    radius: radius,
+    type: ['restaurant']
+  };
 
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
 
 $(document).on("click", ".expand", function () {
-    expandSearch();
+  expandSearch();
 });
 
 function onPlaceChanged() {
-    var place = autocomplete.getPlace();
-    if (place.geometry) {
-        map.panTo(place.geometry.location);
-        map.setZoom(15);
-        search();
-    } else {
-        document.getElementById('autocomplete').placeholder = 'Enter a city';
-    }
+  var place = autocomplete.getPlace();
+  if (place.geometry) {
+    map.panTo(place.geometry.location);
+    map.setZoom(15);
+    search();
+  } else {
+    document.getElementById('autocomplete').placeholder = 'Enter a city';
+  }
 }
 
 
 function search() {
-    var search = {
-        bounds: map.getBounds(),
-        types: ['restaurant']
-    };
+  var search = {
+    bounds: map.getBounds(),
+    types: ['restaurant']
+  };
 
-    places.nearbySearch(search, function (results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            clearResults();
-            clearMarkers();
-            // Create a marker for each hotel found, and
-            // assign a letter of the alphabetic to each marker icon.
-            for (var i = 0; i < results.length; i++) {
-                var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
-                var markerIcon = MARKER_PATH + markerLetter + '.png';
-                // Use marker animation to drop the icons incrementally on the map.
-                markers[i] = new google.maps.Marker({
-                    position: results[i].geometry.location,
-                    animation: google.maps.Animation.DROP,
-                    icon: markerIcon
-                });
-                // If the user clicks a hotel marker, show the details of that hotel
-                // in an info window.
-                markers[i].placeResult = results[i];
-                google.maps.event.addListener(markers[i], 'click', showInfoWindow);
-                setTimeout(dropMarker(i), i * 100);
-                addResult(results[i], i);
-            }
-        }
-    });
+  places.nearbySearch(search, function (results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      clearResults();
+      clearMarkers();
+      // Create a marker for each hotel found, and
+      // assign a letter of the alphabetic to each marker icon.
+      for (var i = 0; i < results.length; i++) {
+        var markerLetter = String.fromCharCode('A'.charCodeAt(0) + (i % 26));
+        var markerIcon = MARKER_PATH + markerLetter + '.png';
+        // Use marker animation to drop the icons incrementally on the map.
+        markers[i] = new google.maps.Marker({
+          position: results[i].geometry.location,
+          animation: google.maps.Animation.DROP,
+          icon: markerIcon
+        });
+        // If the user clicks a hotel marker, show the details of that hotel
+        // in an info window.
+        markers[i].placeResult = results[i];
+        google.maps.event.addListener(markers[i], 'click', showInfoWindow);
+        setTimeout(dropMarker(i), i * 100);
+        addResult(results[i], i);
+      }
+    }
+  });
 }
 
 // todo Needs some work to be able to splice tempArr from placeArr
 
 function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
 
-            var placeId = results[i].place_id;
-            placeArr.push(placeId);
-        }
-
+      var placeId = results[i].place_id;
+      placeArr.push(placeId);
     }
 
-    for (var i = 0; i < placeArr.length; i++) {
-        for (var j = 0; j < placeArr.length; j++) {
-            if (tempArr[i] === placeArr[j]) {
-                placeArr.splice(j, 1);
-            }
-        }
+  }
+
+  for (var i = 0; i < placeArr.length; i++) {
+    for (var j = 0; j < placeArr.length; j++) {
+      if (tempArr[i] === placeArr[j]) {
+        placeArr.splice(j, 1);
+      }
     }
-    placeDetails(placeArr[restIndex]);
+  }
+  placeDetails(placeArr[restIndex]);
 }
 
 
@@ -229,11 +229,11 @@ function callback(results, status) {
 var placeArr = [];
 
 function createMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
-    // *Location for current search results
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+  // *Location for current search results
 
 
 }
@@ -241,39 +241,39 @@ function createMarker(place) {
 
 // *Get place details
 function placeDetails(place) {
-    var request = {
-        placeId: place,
-        fields: ['name', 'rating', 'formatted_phone_number', 'photos', 'website', 'address_components'],
-    };
+  var request = {
+    placeId: place,
+    fields: ['name', 'rating', 'formatted_phone_number', 'photos', 'website', 'address_components'],
+  };
 
-    service.getDetails(request, (place) => {
+  service.getDetails(request, (place) => {
 
-        //* This is where we are going to grab all of the data and set it up on the screen
-        name = place.name;
-        rating = place.rating;
-        if (place.formatted_phone_number) {
-            phone = place.formatted_phone_number;
-        }
-        if (place.photos) {
-            photo = place.photos[0].getUrl({
-                'maxWidth': 300,
-                'maxHeight': 300
-            });
-        } else {
-            photo = "./assets/images/No_image_available.png";
-        }
-        if (place.website) {
-            website = place.website;
-        } else {
-            website = false;
-        }
+    //* This is where we are going to grab all of the data and set it up on the screen
+    name = place.name;
+    rating = place.rating;
+    if (place.formatted_phone_number) {
+      phone = place.formatted_phone_number;
+    }
+    if (place.photos) {
+      photo = place.photos[0].getUrl({
+        'maxWidth': 300,
+        'maxHeight': 300
+      });
+    } else {
+      photo = "./assets/images/No_image_available.png";
+    }
+    if (place.website) {
+      website = place.website;
+    } else {
+      website = false;
+    }
 
-        address1 = place.address_components[0].short_name;
-        address2 = place.address_components[1].short_name;
-        address3 = place.address_components[2].short_name;
+    address1 = place.address_components[0].short_name;
+    address2 = place.address_components[1].short_name;
+    address3 = place.address_components[2].short_name;
 
-        displayRestaurant();
-    });
+    displayRestaurant();
+  });
 }
 
 
@@ -291,15 +291,15 @@ var dilikeArr = [];
 
 
 function createMarker(place) {
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-    google.maps.event.addListener(marker, 'click', function () {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
+  google.maps.event.addListener(marker, 'click', function () {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
 
@@ -307,53 +307,53 @@ var restIndex = 0;
 
 function displayRestaurant() {
 
-    $("#restaurant").empty();
-    var newDiv = $("<div>").addClass("card");
-    var pic = $("<div>").addClass("card-image").append("<img src='" + photo + "' />");
-    var title = $("<span>").addClass("card-title").append(name);
-    var content = $("<div>").addClass("card-content").attr("style", "background-color:goldenrod");
-    var addressCombined = $("<div>").append(address1 + " ").append(address2 + ", ").append(address3);
-    var rate = $("<div>").append("Rating: " + rating);
+  $("#restaurant").empty();
+  var newDiv = $("<div>").addClass("card");
+  var pic = $("<div>").addClass("card-image").attr("style", "background-image:url(" + photo + ")");
+  var title = $("<span>").addClass("card-title").append(name);
+  var content = $("<div>").addClass("card-content").attr("style", "background-color:goldenrod");
+  var addressCombined = $("<div>").append(address1 + " ").append(address2 + ", ").append(address3);
+  var rate = $("<div>").append("Rating: " + rating);
 
-    // todo would like for website to be an <a href> if possible
+  // todo would like for website to be an <a href> if possible
 
-    var aTag = $("<a>")
-    if (website) {
-        aTag.attr("href", website);
-        aTag.attr("target", "_blank");
-        aTag.text("Website")
-        // var web = $("<a href=" + website + ">Website</a>");
+  var aTag = $("<a>")
+  if (website) {
+    aTag.attr("href", website);
+    aTag.attr("target", "_blank");
+    aTag.text("Website")
+    // var web = $("<a href=" + website + ">Website</a>");
 
 
-    }
-    var number = $("<div>").append("Phone: " + phone);
+  }
+  var number = $("<div>").append("Phone: " + phone);
 
-    content.append(title).append(rate).append(aTag).append(number);
-    newDiv.append(pic).append(content);
-    $("#restaurant").append(newDiv);
+  content.append(title).append(rate).append(aTag).append(number);
+  newDiv.append(pic).append(content);
+  $("#restaurant").append(newDiv);
 
 
 }
 var likeIndex = 0;
 
 function likedDiv() {
-    $("#liked-row").append("<div class='col m4 newLiked" + likeIndex + " inner grid-item'>");
-    $(".liked" + likeIndex).appendTo(".newLiked" + likeIndex);
-    $(".card").removeClass("liked");
-    likeIndex++;
+  $("#liked-row").append("<div class='col m4 newLiked" + likeIndex + " inner grid-item'>");
+  $(".liked" + likeIndex).appendTo(".newLiked" + likeIndex);
+  $(".card").removeClass("liked");
+  likeIndex++;
 }
 
 $(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 601) {
-        $("#like").removeClass("valign-wrapper").addClass("center-align");
-        $("#dislike").removeClass("valign-wrapper").addClass("center-align");
-    }
+  var viewportWidth = $(window).width();
+  if (viewportWidth < 601) {
+    $("#like").removeClass("valign-wrapper").addClass("center-align");
+    $("#dislike").removeClass("valign-wrapper").addClass("center-align");
+  }
 });
 $(window).resize(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth > 600) {
-        $("#like").removeClass("center-align").addClass("valign-wrapper");
-        $("#dislike").removeClass("center-align").addClass("valign-wrapper");
-    }
+  var viewportWidth = $(window).width();
+  if (viewportWidth > 600) {
+    $("#like").removeClass("center-align").addClass("valign-wrapper");
+    $("#dislike").removeClass("center-align").addClass("valign-wrapper");
+  }
 });
